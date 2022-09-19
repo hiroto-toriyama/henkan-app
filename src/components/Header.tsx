f94a336f50349f73abd9cbfacb2670034f0efa21
h.toriyama <toriyamahiroto1128@gmail.com>
2022-09-19 11:05:43 +0900

    react-iconsを導入

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { AiOutlineDown } from "react-icons/ai";
 
 const Header = () => {
   return (
@@ -12,7 +13,7 @@ const Header = () => {
           <li tabIndex={0}>
             <a>
               Parent
-              <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+              <AiOutlineDown />
             </a>
             <ul className="p-2 bg-base-100">
               <li><a>Submenu 1</a></li>
