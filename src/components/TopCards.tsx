6d773fed9d115f9431416d9825276bda26abc15d
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-04 16:04:07 +0900

    カードの色がランダムになるようにした(ただしあまり良い見た目じゃなかったのでコメントアウト)

--- src/components/TopCards.tsx
+++ src/components/TopCards.tsx
@@ -4,12 +4,29 @@ export type TopCardsPropsType = {
   topCardsProps: TopCardPropsType[] 
 };
 
+// bg-primary text-primary-content
+// bg-secondary text-secondary-content
+// bg-accent text-accent-content
+// bg-neutral text-neutral-content
+
 const TopCards = ({topCardsProps}: TopCardsPropsType) => {
+  const colorList = [
+    "primary",
+    "secondary",
+    "accent",
+    "neutral"
+  ];
+
   return (
     <div className="flex flex-wrap my-5 -m-5">
       {
-        topCardsProps.map((topCardProps) => {
-          return <TopCard title={topCardProps.title} text={topCardProps.text} linkTo={topCardProps.linkTo} />;
+        topCardsProps.map((topCardProps, index) => {
+          return <TopCard title={topCardProps.title}
+                          text={topCardProps.text}
+                          linkTo={topCardProps.linkTo}
+                          // color={colorList[Math.floor(Math.random()*colorList.length)]}
+                          key={index}
+                  />;
         })
       }
     </div>
